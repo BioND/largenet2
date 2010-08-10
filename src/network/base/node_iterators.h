bee726e1afda430be2ae1aed7fa133fef0e05397
Gerd Zschaler <gerd@gzschaler.de>
2010-08-10 15:41:27 +0200

    Felix' fix for iterators to work in undirected case

--- src/network/base/node_iterators.h
+++ src/network/base/node_iterators.h
@@ -38,25 +38,31 @@ public:
 	typedef typename choose_type<is_const, const Node*, Node*>::type
 			node_pointer;
 
-	explicit NodeOutNeighborIterator(const Iterator& i) :
-		it_(i)
+	explicit NodeOutNeighborIterator(const Iterator& i, const node_id_t nid) :
+		it_(i), nid_(nid)
 	{
 	}
 	NodeOutNeighborIterator()
 	{
 	}
-	NodeOutNeighborIterator(const NodeOutNeighborIterator& other) :
-		it_(other.it_)
+	NodeOutNeighborIterator(const NodeOutNeighborIterator& other, const node_id_t nid) :
+		it_(other.it_), nid_(nid)
 	{
 	}
 	typename choose_type<is_const, const node_id_t, node_id_t>::type operator*()
 	{
-		return (*it_)->target()->id();
+		if ((*it_)->isDirected())
+			return (*it_)->target()->id();
+		else
+			return ((nid_ == (*it_)->target()->id()) ? (*it_)->source()->id() : (*it_)->target()->id());
 	}
 
 	node_pointer operator->()
 	{
-		return (*it_)->target();
+		if ((*it_)->isDirected())
+			return (*it_)->target();
+		else
+			return ((nid_ == (*it_)->target()->id()) ? (*it_)->source() : (*it_)->target());
 	}
 
 	NodeOutNeighborIterator& operator=(const NodeOutNeighborIterator& i)
@@ -100,6 +106,7 @@ public:
 	}
 private:
 	Iterator it_;
+	node_id_t nid_;
 };
 
 template<class Iterator, bool is_const = false>
