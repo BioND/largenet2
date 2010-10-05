a653fedd2904e32a1abcd4cb70749f0983751a27
Gerd Zschaler <gerd@gzschaler.de>
2010-10-05 16:31:07 +0200

    Fixed NodeInNeighborIterator

--- src/network/base/MultiNode.h
+++ src/network/base/MultiNode.h
@@ -18,24 +18,41 @@ public:
 	MultiNode(node_id_t id) : Node(id) {}
 	virtual ~MultiNode() {}
 
-	virtual degree_t outDegree() const { return outEdges_.size(); }
-	virtual degree_t inDegree() const { return inEdges_.size(); }
-	virtual bool hasInEdge(const Edge* e) const { /* here be dragons */ return inEdges_.find(const_cast<Edge*> (e)) != inEdges_.end(); }
-	virtual bool hasOutEdge(const Edge* e) const {/* here be dragons */  return outEdges_.find(const_cast<Edge*> (e)) != outEdges_.end(); }
-	virtual bool hasEdgeTo(const Node* n) const;
-	virtual bool hasEdgeFrom(const Node* n) const;
-	virtual Edge* edgeTo(const Node* n) const;
-	virtual Edge* edgeFrom(const Node* n) const;
-	virtual edge_iterator_range outEdges() const { return std::make_pair(outEdges_.begin(), outEdges_.end()); }
-	virtual edge_iterator_range inEdges() const { return std::make_pair(inEdges_.begin(), inEdges_.end()); }
-	virtual OutNeighborIteratorRange outNeighbors() { return std::make_pair(OutNeighborIterator(outEdges_.begin(), id()), OutNeighborIterator(outEdges_.end(), id())); }
-	virtual ConstOutNeighborIteratorRange outNeighbors() const { return std::make_pair(ConstOutNeighborIterator(outEdges_.begin(), id()), ConstOutNeighborIterator(outEdges_.end(), id())); }
-	virtual InNeighborIteratorRange inNeighbors() { return std::make_pair(InNeighborIterator(inEdges_.begin()), InNeighborIterator(inEdges_.end())); }
-	virtual ConstInNeighborIteratorRange inNeighbors() const { return std::make_pair(ConstInNeighborIterator(inEdges_.begin()), ConstInNeighborIterator(inEdges_.end())); }
+	degree_t outDegree() const { return outEdges_.size(); }
+	degree_t inDegree() const { return inEdges_.size(); }
+	bool hasInEdge(const Edge* e) const { /* here be dragons */ return inEdges_.find(const_cast<Edge*> (e)) != inEdges_.end(); }
+	bool hasOutEdge(const Edge* e) const {/* here be dragons */  return outEdges_.find(const_cast<Edge*> (e)) != outEdges_.end(); }
+	bool hasEdgeTo(const Node* n) const;
+	bool hasEdgeFrom(const Node* n) const;
+	Edge* edgeTo(const Node* n) const;
+	Edge* edgeFrom(const Node* n) const;
+	edge_iterator_range outEdges() const { return std::make_pair(outEdges_.begin(), outEdges_.end()); }
+	edge_iterator_range inEdges() const { return std::make_pair(inEdges_.begin(), inEdges_.end()); }
+	OutNeighborIteratorRange outNeighbors()
+	{
+		return std::make_pair(OutNeighborIterator(outEdges_.begin(), id()),
+				OutNeighborIterator(outEdges_.end(), id()));
+	}
+	ConstOutNeighborIteratorRange outNeighbors() const
+	{
+		return std::make_pair(
+				ConstOutNeighborIterator(outEdges_.begin(), id()),
+				ConstOutNeighborIterator(outEdges_.end(), id()));
+	}
+	InNeighborIteratorRange inNeighbors()
+	{
+		return std::make_pair(InNeighborIterator(inEdges_.begin(), id()),
+				InNeighborIterator(inEdges_.end(), id()));
+	}
+	ConstInNeighborIteratorRange inNeighbors() const
+	{
+		return std::make_pair(ConstInNeighborIterator(inEdges_.begin(), id()),
+				ConstInNeighborIterator(inEdges_.end(), id()));
+	}
 
 protected:
-	virtual void registerEdge(const Edge* e);
-	virtual void unregisterEdge(const Edge* e);
+	void registerEdge(const Edge* e);
+	void unregisterEdge(const Edge* e);
 
 private:
 	edge_set outEdges_, inEdges_;
