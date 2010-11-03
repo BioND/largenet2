a3b047808d6dc95749324f4f3170d520ac92010c
Gerd Zschaler <gerd@gzschaler.de>
2010-11-03 18:41:13 +0100

    Power-law out-degree network generator

--- src/network/generators/generators.h
+++ src/network/generators/generators.h
@@ -9,14 +9,46 @@
 
 #include "../largenet.h"
 #include <boost/unordered_set.hpp>
+#include <vector>
 #include <cassert>
 #include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <utility>
 
 namespace largenet
 {
 
 namespace generators
 {
+
+namespace util
+{
+
+template<class U, class V, class RandomGen>
+U& random_from(V& container, RandomGen& rng)
+{
+	int i = rng.IntFromTo(0, container.size() - 1);
+	return container[i];
+}
+
+template<class _Iter, class RandomGen>
+_Iter random_from(_Iter begin, _Iter end, RandomGen& rng)
+{
+	if (begin == end)
+		return end;
+	int i = rng.IntFromTo(0, std::distance(begin, end) - 1);
+	std::advance(begin, i);
+	assert(begin != end);
+	return begin;
+}
+
+template<class _Iter, class RandomGen>
+_Iter random_from(std::pair<_Iter, _Iter> range, RandomGen& rng)
+{
+	return random_from(range.first, range.second, rng);
+}
+}
 
 template<class RandomGen>
 void randomGnm(Graph& g, node_size_t numNodes, edge_size_t numEdges,
@@ -32,6 +64,7 @@ void randomGnm(Graph& g, node_size_t numNodes, edge_size_t numEdges,
 
 	if (numNodes < 1)
 		return;
+	// FIXME does this algorithm what it is expected to do when creating directed edges??
 	node_size_t max_edges = numNodes * (numNodes - 1) / 2; // undirected
 	assert(numEdges <= max_edges);
 
@@ -49,7 +82,7 @@ void randomGnm(Graph& g, node_size_t numNodes, edge_size_t numEdges,
 					std::sqrt(0.25 + 2.0 * edge_index) - 0.5));
 			current_edge.second = static_cast<node_id_t> (edge_index
 					- current_edge.first * (current_edge.first - 1) / 2);
-			if (current_edge.first == current_edge.second)	// disallow self-loops
+			if (current_edge.first == current_edge.second) // disallow self-loops
 				continue;
 			if (edges.find(current_edge) == edges.end())
 			{
@@ -73,7 +106,7 @@ template<class RandomGen>
 void randomGnp(Graph& g, node_size_t numNodes, double edgeProb, RandomGen& rng)
 {
 	// FIXME this seems broken
-	throw ("Not yet implented");
+	throw("Not yet implented");
 	/*
 	 * efficient G(n,p) from Phys. Rev. E 71, 036113 (2005)
 	 */
@@ -136,6 +169,63 @@ void randomBA(Graph& g, node_size_t numNodes, edge_size_t m, RandomGen& rng)
 
 	for (size_t i = 0; i < numNodes * m; ++i)
 		g.addEdge(nodes[2 * i], nodes[2 * i + 1], false); // undirected
+}
+
+/**
+ * Create directed graph with power law out-degree distribution with exponent @p exponent
+ * and Poisson in-degree distribution.
+ * @param g Graph object to hold graph, must be empty
+ * @param numNodes Total number of nodes of final graph
+ * @param exponent power law exponent in out-degree distribution @f$ p(k) = k^(-exponent) @f$
+ * @param rng Random number generator providing uniform IntFromTo(int low, int high)
+ */
+template<class RandomGen>
+void randomOutDegreePowerlaw(Graph& g, node_size_t numNodes, double exponent,
+		RandomGen& rng)
+{
+	if (g.numberOfNodes() != 0)
+		throw("Need empty graph in randomBA");
+
+	double normalization = 0;
+	for (int i = numNodes; i >= 1; --i)
+	{
+		normalization += pow(i, -exponent);
+	}
+	normalization = 1.0 / normalization;
+
+	std::vector<node_size_t> degdist(numNodes + 1, 0);
+	double remain = 0;
+	for (size_t i = numNodes; i >= 1; --i)
+	{
+		degdist[i] = 0;
+		remain += numNodes * normalization * pow(i, -exponent);
+		degdist[i] = floor(remain);
+		remain -= floor(remain);
+	}
+	degdist[0] = round(remain);
+
+	while (g.numberOfNodes() < numNodes)
+		g.addNode();
+
+	std::vector<node_id_t> nodes(numNodes, 0);
+	Graph::NodeIteratorRange nd = g.nodes();
+	std::transform(nd.first, nd.second, nodes.begin(), mem_fun_ref(&Node::id));
+
+	for (size_t i = numNodes; i > 0; --i)
+	{
+		for (node_size_t n = degdist[i]; n > 0; n--)
+		{
+			node_id_t cur_id = nodes.back();
+			nodes.pop_back();
+			for (node_size_t k = 0; k < i; ++k)
+			{
+				Graph::NodeIterator nit = util::random_from(g.nodes(), rng);
+				while (g.isEdge(cur_id, nit.id()))
+					nit = util::random_from(g.nodes(), rng);
+				g.addEdge(cur_id, nit.id(), true);
+			}
+		}
+	}
 }
 
 }
