68ec99712b263f69cfcb02d840c97cf951ff7a40
Gerd Zschaler <gerd@gzschaler.de>
2010-11-19 16:52:23 +0100

    Started motif count functions

--- src/network/motifs/detail/motif_construction.cpp
+++ src/network/motifs/detail/motif_construction.cpp
@@ -8,6 +8,8 @@
 #include "motif_construction.h"
 #include "../NodeMotif.h"
 #include "../LinkMotif.h"
+#include "../TripleMotif.h"
+#include "../QuadLineMotif.h"
 
 namespace largenet
 {
@@ -17,51 +19,92 @@ namespace detail
 {
 
 template<>
-void constructAllMotifs(std::set<NodeMotif>& m, node_state_size_t states, bool directed)
+void constructAllMotifs(std::set<NodeMotif>& m, node_state_size_t states,
+		bool directed)
 {
 	for (node_state_t n = 0; n < states; ++n)
 		m.insert(NodeMotif(n));
 }
 
 template<>
-void constructAllMotifs(std::set<LinkMotif>& m, node_state_size_t states, bool directed)
+void constructAllMotifs(std::set<LinkMotif>& m, node_state_size_t states,
+		bool directed)
 {
 	for (node_state_t a = 0; a < states; ++a)
 		for (node_state_t b = 0; b < states; ++b)
 			m.insert(LinkMotif(a, b, directed));
 }
 
+template<>
+void constructAllMotifs(std::set<TripleMotif>& m, node_state_size_t states,
+		bool directed)
+{
+	if (!directed)
+	{
+		for (node_state_t a = 0; a < states; ++a)
+			for (node_state_t b = 0; b < states; ++b)
+				for (node_state_t c = 0; c < states; ++c)
+					m.insert(TripleMotif(a, b, c, TripleMotif::NO_DIR));
+	}
+	else
+	{
+		for (node_state_t a = 0; a < states; ++a)
+			for (node_state_t b = 0; b < states; ++b)
+				for (node_state_t c = 0; c < states; ++c)
+				{
+					m.insert(TripleMotif(a, b, c, TripleMotif::LCR));
+					m.insert(TripleMotif(a, b, c, TripleMotif::LRC));
+					m.insert(TripleMotif(a, b, c, TripleMotif::RCL));
+					m.insert(TripleMotif(a, b, c, TripleMotif::CLR));
+				}
+
+	}
+}
+
+template<>
+void constructAllMotifs(std::set<QuadLineMotif>& m, node_state_size_t states,
+		bool directed)
+{
+	if (!directed)
+	{
+		for (node_state_t a = 0; a < states; ++a)
+			for (node_state_t b = 0; b < states; ++b)
+				for (node_state_t c = 0; c < states; ++c)
+					for (node_state_t d = 0; d < states; ++d)
+						m.insert(QuadLineMotif(a, b, c, d,
+								QuadLineMotif::NO_DIR));
+	}
+	else
+	{
+		for (node_state_t a = 0; a < states; ++a)
+			for (node_state_t b = 0; b < states; ++b)
+				for (node_state_t c = 0; c < states; ++c)
+					for (node_state_t d = 0; d < states; ++d)
+					{
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::RLINE));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::C_IN));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::C_OUT));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::B_IN));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::B_OUT));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::B_IN_C_OUT));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::B_OUT_C_IN));
+						m.insert(QuadLineMotif(a, b, c, d, QuadLineMotif::LLINE));
+					}
+	}
+}
+
 /*
-template<>
-void constructAllMotifs(std::set<TripleMotif>& m, node_state_size_t states)
-{
-	for (node_state_t a = 0; a < states; ++a)
-		for (node_state_t b = 0; b < states; ++b)
-			for (node_state_t c = 0; c < states; ++c)
-				m.insert(TripleMotif(a, b, c));
-}
+ template<>
+ void constructAllMotifs(std::set<QuadStarMotif>& m, node_state_size_t states)
+ {
+ for (node_state_t a = 0; a < states; ++a)
+ for (node_state_t b = 0; b < states; ++b)
+ for (node_state_t c = 0; c < states; ++c)
+ for (node_state_t d = 0; d < states; ++d)
+ m.insert(QuadStarMotif(a, b, c, d));
+ }
 
-template<>
-void constructAllMotifs(std::set<QuadLineMotif>& m, node_state_size_t states)
-{
-	for (node_state_t a = 0; a < states; ++a)
-		for (node_state_t b = 0; b < states; ++b)
-			for (node_state_t c = 0; c < states; ++c)
-				for (node_state_t d = 0; d < states; ++d)
-					m.insert(QuadLineMotif(a, b, c, d));
-}
-
-template<>
-void constructAllMotifs(std::set<QuadStarMotif>& m, node_state_size_t states)
-{
-	for (node_state_t a = 0; a < states; ++a)
-		for (node_state_t b = 0; b < states; ++b)
-			for (node_state_t c = 0; c < states; ++c)
-				for (node_state_t d = 0; d < states; ++d)
-					m.insert(QuadStarMotif(a, b, c, d));
-}
-
-*/
+ */
 }
 }
 }
