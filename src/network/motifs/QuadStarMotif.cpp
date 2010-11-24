17169ccd5d81fb42281df13e84192fa97efa8e9d
Gerd Zschaler <gerd@gzschaler.de>
2010-11-24 16:14:02 +0100

    Fixed canonical ordering of QuadStarMotif

--- src/network/motifs/QuadStarMotif.cpp
+++ src/network/motifs/QuadStarMotif.cpp
@@ -6,12 +6,66 @@
 
 #include "QuadStarMotif.h"
 #include <sstream>
+#include <stdexcept>
 
 namespace largenet
 {
 
 namespace motifs
 {
+
+QuadStarMotif::QuadStarMotif(node_state_t center, node_state_t a,
+		node_state_t b, node_state_t c, Direction dir) :
+	center_(center), a_(a), b_(b), c_(c), dir_(dir)
+{
+	// order canonically
+	if (b_ < a_)
+	{
+		std::swap(a_, b_);
+		ArrowDir a_dir = (dir_ & a_in) ? a_in : a_out;
+		ArrowDir b_dir = (dir_ & b_in) ? b_in : b_out;
+		int temp = (dir_ ^ a_dir) ^ b_dir;
+		dir_ = static_cast<Direction> (temp | (a_dir >> 4) | (b_dir << 4));
+	}
+	if (c_ < b_)
+	{
+		std::swap(c_, b_);
+		ArrowDir b_dir = (dir_ & b_in) ? b_in : b_out;
+		ArrowDir c_dir = (dir_ & c_in) ? c_in : c_out;
+		int temp = (dir_ ^ b_dir) ^ c_dir;
+		dir_ = static_cast<Direction> (temp | (b_dir >> 4) | (c_dir << 4));
+	}
+	if (b_ < a_)
+	{
+		std::swap(a_, b_);
+		ArrowDir a_dir = (dir_ & a_in) ? a_in : a_out;
+		ArrowDir b_dir = (dir_ & b_in) ? b_in : b_out;
+		int temp = (dir_ ^ a_dir) ^ b_dir;
+		dir_ = static_cast<Direction> (temp | (a_dir >> 4) | (b_dir << 4));
+	}
+
+	// set canonical (smallest) direction for symmetric states
+	if (a_ == b_)
+	{
+		if (b_ == c_)
+		{
+			if (dir_ == B_OUT || dir_ == A_OUT)
+				dir_ = C_OUT;
+			else if (dir_ == AB_OUT || dir_ == AC_OUT)
+				dir_ = BC_OUT;
+		} else if (dir_ == AC_OUT)
+			dir_ = BC_OUT;
+		else if (dir_ == A_OUT)
+			dir_ = B_OUT;
+	}
+	else if (b_ == c_) // a is smaller
+	{
+		if (dir_ == AB_OUT)
+			dir_ = AC_OUT;
+		else if (dir_ == B_OUT)
+			dir_ = C_OUT;
+	}
+}
 
 bool operator==(const QuadStarMotif& A, const QuadStarMotif& B)
 {
@@ -21,10 +75,6 @@ bool operator==(const QuadStarMotif& A, const QuadStarMotif& B)
 
 bool operator<(const QuadStarMotif& A, const QuadStarMotif& B)
 {
-	if (A.dir() < B.dir())
-		return true;
-	else if (A.dir() == B.dir())
-	{
 		if (A.center() < B.center())
 			return true;
 		else if (A.center() == B.center())
@@ -39,10 +89,14 @@ bool operator<(const QuadStarMotif& A, const QuadStarMotif& B)
 				{
 					if (A.c() < B.c())
 						return true;
+					else if (A.c() == B.c())
+					{
+						if (A.dir() < B.dir())
+							return true;
+					}
 				}
 			}
 		}
-	}
 	return false;
 }
 
